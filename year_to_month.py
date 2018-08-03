2c0810ee270cf5ad3815bfd1e1259d07cb28a315
anhauxiang <1342181530@qq.com>
2018-08-03 15:26:56 +0800

    a

---
+++ year_to_month.py
@@ -0,0 +1,16 @@
+
+from datetime import datetime, timedelta
+from calendar import monthrange
+
+
+def year_to_month(cls, start_year, end_year, back_secend=0):
+    month_range_list = []
+    start_time = datetime(start_year, 1, 1)
+    for year in range(start_year, end_year):
+        for month in range(1, 13, 1):
+            days = monthrange(year, month)[1]
+            delta = timedelta(days=days)
+            next_time = start_time + delta
+            month_range_list.append([start_time, next_time - timedelta(seconds=back_secend)])
+            start_time = next_time
+    return month_range_list
