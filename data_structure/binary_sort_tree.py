92c002d6b3b7ee0a10319fc8d1b7b5235cb2d558
anhuaxiang <1342181530@qq.com>
2018-10-26 17:19:12 +0800

    feature: 二叉排序树

---
+++ data_structure/binary_sort_tree.py
@@ -0,0 +1,113 @@
+# coding:utf-8
+
+
+class BinarySearchTree:
+
+    def __init__(self):
+        self.root = None
+        self.size = 0
+
+    def __len__(self):
+        return self.size
+
+    def __iter__(self):
+        return self.root.__iter__()
+
+    def __setitem__(self, key, value):
+        self.put(key, value)
+
+    def __getitem__(self, key):
+        return self.get(key)
+
+    def __contains__(self, key):
+        if self._get(key, self.root):
+            return True
+        else:
+            return False
+
+    def length(self):
+        return self.size
+
+    def _put(self, key, val, current_node):
+        if key < current_node.key:
+            if current_node.has_left_child():
+                self._put(key, val, current_node.left_child)
+            else:
+                current_node.left_child = TreeNode(key, val, parent=current_node)
+        else:
+            if current_node.has_right_child():
+                self._put(key, val, current_node.right_child)
+            else:
+                current_node.right_child = TreeNode(key, val, parent=current_node)
+
+    def put(self, key, val):
+        if self.root:
+            self._put(key, val, self.root)
+        else:
+            self.root = TreeNode(key, val)
+        self.size = self.size + 1
+
+    def _get(self, key, current_node):
+        if not current_node:
+            return None
+        elif current_node.key == key:
+            return current_node
+        elif key < current_node.key:
+            return self._get(key, current_node.left_child)
+        else:
+            return self._get(key, current_node.right_child)
+
+    def get(self, key):
+        if self.root:
+            res = self._get(key, self.root)
+            if res:
+                return res.val
+            else:
+                return None
+        return None
+
+
+class TreeNode:
+
+    def __init__(self, key, val, left=None, right=None, parent=None):
+        self.key = key
+        self.val = val
+        self.left_child = left
+        self.right_child = right
+        self.parent = parent
+
+    def has_left_child(self):
+        return self.left_child
+
+    def has_right_child(self):
+        return self.right_child
+
+    def is_left_child(self):
+        return self.parent and self.parent.left_child == self
+
+    def is_right_child(self):
+        return self.parent and self.parent.right_child == self
+
+    def is_root(self):
+        return not self.parent
+
+    def is_leaf(self):
+        return not (self.left_child or self.right_child)
+
+    def has_any_child(self):
+        return self.left_child or self.right_child
+
+    def has_both_child(self):
+        return self.left_child and self.right_child
+
+    def replace_node_data(self, key, val, lc, rc):
+        self.key = key
+        self.val = val
+        self.left_child = lc
+        self.right_child = rc
+        if self.has_left_child():
+            self.left_child.parent = self
+        if self.has_right_child():
+            self.right_child.parent = self
+
+
